dd4923b870d54e71aa3184456e558b2bd0646d66
Rafael Barbosa <rbarbosa@tijob.tech>
2021-04-10 14:06:23 -0300

    tela para mostrar botao

---
+++ src/screens/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Select from '../components/select';
+
+const Home: React.FC = () => {
+  return (
+    <View style={{padding: 20}}>
+      <Select />
+    </View>
+  );
+};
+
+export default Home;
