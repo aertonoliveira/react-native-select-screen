d8cfcf4b4d5f0c854ab4145831c2229d3f724d18
Rafael Barbosa <rbarbosa@tijob.tech>
2021-04-10 14:06:07 -0300

    adicionada rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,19 @@
+import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import Home from '../screens/Home';
+import ListItems from '../screens/ListItems';
 
 const Stack = createStackNavigator();
 
 function MyStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{headerShown: false}}>
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Notifications" component={Notifications} />
+      <Stack.Screen name="ListItems" component={ListItems} />
     </Stack.Navigator>
   );
 }
+
+export default MyStack;
